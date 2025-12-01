66a96fa263fcc72e3ed19cdef98e8d95de46da92
ydt <230103074@gmail.com>
2025-12-01 17:03:11 +0500

    first commit

---
+++ src/test/java/ai/devopsfinal/DevopsFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package ai.devopsfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsFinalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
